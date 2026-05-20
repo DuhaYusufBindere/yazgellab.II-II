296933d32beabf71b7d6296029174ddf35bf3014
Duha Yusuf Bindere <duhayusuf2005@gmail.com>
2026-05-20 17:36:33 +0300

    Veri kaymasını engellicek fonksiyon yazıldı

--- src/data/batadal_loader.py
+++ src/data/batadal_loader.py
@@ -50,3 +50,25 @@ def separate_target(df):
     
     X = df.drop(columns=[target_col])
     return X, y
+
+def ensure_temporal_order(df):
+    """
+    Returns:
+        pd.DataFrame: Kronolojik sırası garanti edilmiş dataframe.
+    """
+    if "DATETIME" in df.columns:
+        # Tarih formatı: '04/07/16 00' (Gün/Ay/Yıl Saat)
+        temp_dt = pd.to_datetime(df["DATETIME"], format="%d/%m/%y %H")
+        if not temp_dt.is_monotonic_increasing:
+            print("⚠️ Uyarı: Veri kronolojik sırada değil! Sıralanıyor...")
+            df = df.iloc[temp_dt.argsort()].reset_index(drop=True)
+        else:
+            print("✅ Veri sırası kontrol edildi: Kronolojik sıra (Temporal Order) korundu.")
+    else:
+        if not df.index.is_monotonic_increasing:
+            print("⚠️ Uyarı: Veri indeks sırası bozuk! Sıralanıyor...")
+            df = df.sort_index().reset_index(drop=True)
+        else:
+            print("✅ Veri sırası kontrol edildi: İndeks sıralaması korundu.")
+            
+    return df
