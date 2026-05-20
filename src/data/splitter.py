f77f70424e244d24b6663a89099c0ff0d710affc
Duha Yusuf Bindere <duhayusuf2005@gmail.com>
2026-05-20 23:30:52 +0300

    BATADAL için Temporal veri bölme fonksiyonu eklendi

--- src/data/splitter.py
+++ src/data/splitter.py
@@ -9,3 +9,20 @@ def skab_group_kfold(X, y, groups, config=None):
     gkf = GroupKFold(n_splits=n_splits)
     splits = list(gkf.split(X, y, groups=groups))
     return splits
+
+
+def batadal_train_split(X, y, config=None):
+    
+    if config is None:
+        config = load_config()
+    
+    train_split = config["data"].get("train_split", 0.6)
+    
+    n_samples = len(X)
+    train_end = int(n_samples * train_split)
+    
+    X_train = X.iloc[:train_end]
+    y_train = y.iloc[:train_end]
+    
+    return X_train, y_train
+
