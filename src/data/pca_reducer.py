98be79fb2a1650a25df323516c627cb41901dba0
ahmetoz1 <ahmetoz212@gmail.com>
2026-05-21 16:33:44 +0300

    PCA için data leakage korumalı fit fonksiyonu eklendi

--- src/data/pca_reducer.py
+++ src/data/pca_reducer.py
@@ -8,3 +8,8 @@ def get_pca(config=None):
     n_components = config.get("automata", {}).get("pca_n_components", 1)
     return PCA(n_components=n_components)
 
+
+def fit_pca_on_train(pca, X_train):
+    numeric_cols = X_train.select_dtypes(include=["number"]).columns
+    pca.fit(X_train[numeric_cols])
+    return pca
