eba9c12c1c6cdda91fc7bf1b3b03b9f5e45a3d07
ahmetoz1 <ahmetoz212@gmail.com>
2026-05-22 15:55:15 +0300

    PAA değerlerini sembollere çeviren SAX dönüşüm fonksiyonu eklendi

--- src/models/sax.py
+++ src/models/sax.py
@@ -6,3 +6,16 @@ def compute_breakpoints(alphabet_size):
     quantiles = np.linspace(0, 1, alphabet_size + 1)[1:-1]
     breakpoints = norm.ppf(quantiles)
     return breakpoints
+
+
+def sax_transform(paa_values, alphabet_size):
+    breakpoints = compute_breakpoints(alphabet_size)
+    symbols = []
+    
+    for val in paa_values:
+        # val'ın düştüğü breakpointi bul
+        idx = np.searchsorted(breakpoints, val)
+        symbol = chr(97 + idx) 
+        symbols.append(symbol)
+        
+    return "".join(symbols)
