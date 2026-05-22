2109d904ff477a749ce2b50def736ff8a57cac23
ahmetoz1 <ahmetoz212@gmail.com>
2026-05-22 16:27:34 +0300

    Pattern mesafe fonksiyonu eklendi
    İki pattern arasındaki Levenshtein mesafesini hesaplayan fonksiyon eklendi

--- src/models/unseen.py
+++ src/models/unseen.py
@@ -1,2 +1,10 @@
+import Levenshtein
+
+
 def is_unseen_pattern(pattern, state_dictionary):
     return pattern not in state_dictionary
+
+
+def compute_levenshtein_distance(pattern1, pattern2):
+    return Levenshtein.distance(pattern1, pattern2)
+
