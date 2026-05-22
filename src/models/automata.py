64817138d388a3170a129dde2f2df1c49b28e81a
ahmetoz1 <ahmetoz212@gmail.com>
2026-05-22 16:08:01 +0300

    Durumlar arası geçiş frekanslarını sayan fonksiyon eklendi

--- src/models/automata.py
+++ src/models/automata.py
@@ -1,6 +1,5 @@
 from collections import defaultdict
 
-
 def extract_patterns(symbolic_sequence, window_size):
     patterns = []
     n = len(symbolic_sequence)
@@ -12,4 +11,13 @@ def extract_patterns(symbolic_sequence, window_size):
 
 def build_state_dictionary(patterns):
     return set(patterns)
+
+
+def count_transitions(patterns):
+    counts = defaultdict(lambda: defaultdict(int))
+    for i in range(len(patterns) - 1):
+        curr_state = patterns[i]
+        next_state = patterns[i + 1]
+        counts[curr_state][next_state] += 1
+    return counts
 
