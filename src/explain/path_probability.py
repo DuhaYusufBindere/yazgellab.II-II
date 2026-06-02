e704e0debd0d1fceee5aad89512d386d9d71c61c
Duha Yusuf Bindere <duhayusuf2005@gmail.com>
2026-06-02 15:17:03 +0300

    Smoothing veya Underflow Koruması Eklendi

--- src/explain/path_probability.py
+++ src/explain/path_probability.py
@@ -1,12 +1,14 @@
-def calculate_path_probability(test_patterns, transition_probs):
-    """                   
+import math
+
+def calculate_path_probability(test_patterns, transition_probs, use_log=True, epsilon=1e-8):
+    """                
     Returns:
-        float: Toplam yol olasılığı.
+        float: Toplam yol olasılığı (use_log=True ise log olasılık değeri).
     """
     if not test_patterns or len(test_patterns) < 2:
-        return 1.0
+        return 0.0 if use_log else 1.0
         
-    total_prob = 1.0
+    total_prob = 0.0 if use_log else 1.0
     
     for i in range(len(test_patterns) - 1):
         curr_state = test_patterns[i]
@@ -14,6 +16,15 @@ def calculate_path_probability(test_patterns, transition_probs):
         
         # Eğer sözlükte geçiş varsa olasılığını al, yoksa 0.0 olarak kabul et
         prob = transition_probs.get(curr_state, {}).get(next_state, 0.0)
-        total_prob *= prob
+        
+        # Smoothing (0 olasılıkları engelleme)
+        if prob <= 0.0:
+            prob = epsilon
+            
+        # Underflow Koruması (Log Olasılık)
+        if use_log:
+            total_prob += math.log(prob)
+        else:
+            total_prob *= prob
         
     return total_prob
